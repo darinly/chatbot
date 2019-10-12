747ccb6d2ad15d0b7850814489623208d6c0df8f
darinly <31277527+darinly@users.noreply.github.com>
2019-10-12 13:23:29 +0800

    [Update] Update config and memory module

---
+++ memory/src/main/java/com/darin/chatbot/memory/MemoryApplication.java
@@ -0,0 +1,15 @@
+package com.darin.chatbot.memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryApplication.class, args);
+    }
+
+}
